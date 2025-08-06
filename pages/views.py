b2308e4ff66f87eb676d19274e4853fc798848cb
LereRentesca <esteban.renteria@uabc.edu.mx>
2025-08-05 18:50:09 -0700

    Class #2

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 # Create your views here.
+# Class-based views
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+# Function-based views
+# def about_view(request):
+#     return render(request, "pages/about.html")
